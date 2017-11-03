81ab2638619990103aedfaeca8c36f33c67a686a
fluidbyte <kent@fluidbyte.net>
2017-11-03 09:48:35 -0500

    feat(loaders): adds dataloader dep and sets up loaders for users and groups

--- server/index.js
+++ server/index.js
@@ -3,6 +3,16 @@ const bodyParser = require('body-parser')
 const graphQLHTTP = require('express-graphql')
 const app = express()
 const schema = require('./schema')
+const DataLoader = require('dataloader')
+
+const db = require('./db')
+
+// ##############################################
+// Abstract some reusable methods
+
+const getUserById = (id) => db.readUserById(id)
+
+const getGroupById = (id) => db.readGroupById(id)
 
 // ##############################################
 // Statics and setup
@@ -15,9 +25,22 @@ app.use(bodyParser.json())
 // ##############################################
 // Use GraphQL
 
-app.use(graphQLHTTP({
-  schema,
-  graphiql: true
+app.use(graphQLHTTP((req) => {
+  const userLoader = new DataLoader(
+    keys => Promise.all(keys.map(getUserById))
+  )
+  const groupLoader = new DataLoader(
+    keys => Promise.all(keys.map(getGroupById))
+  )
+  const loaders = {
+    user: userLoader,
+    group: groupLoader
+  }
+  return {
+    context: { loaders },
+    schema,
+    graphiql: true
+  }
 }))
 
 
